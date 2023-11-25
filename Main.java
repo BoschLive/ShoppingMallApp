af95d4610393362bd2e36534c6104baeadf2f9a2
BoschLive <116604832+BoschLive@users.noreply.github.com>
2023-11-24 21:39:39 -0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
+import java.util.ArrayList;
+import java.util.Optional;
 import java.util.Scanner;
 import java.util.Arrays;
+
+
 //Collection of classes contributing to signing a user in
 class Interface{
     String email, PW;
@@ -64,6 +68,13 @@ class RegisteredUsers{
     };
 }
 
+
+
+//Classes for sales reports
+class SalesData{
+    
+}
+
 //Collection of classes contributing to Displaying and Interacting with Item Information
 class Item{
     int itemID, itemQty;
@@ -78,6 +89,25 @@ class Item{
         this.itemQty = itemQty;
     }
 
+    public static void addToWishlist(int itemID) {
+        if (CheckStock.checkStock(itemID)) {
+            // If in stock, find the item and add it to the wishlist
+            for (Item item : items) {
+                if (item.itemID == itemID) {
+                    wishlist.add(item);
+                    System.out.println(item.itemName + " added to the wishlist.");
+                    CustomerFrontend frontend = new CustomerFrontend();
+                    frontend.Frontend(items);
+                    return;
+                }
+            }
+        } else {
+            System.out.println("Item with ID " + itemID + " is out of stock. Cannot be added to the wishlist.");
+        }
+    }
+
+    static ArrayList<Item> wishlist = new ArrayList<>();
+
     static Item[] items = {
             new Item(1, "chair", "A chair", 55.00, 5),
             new Item(2, "couch", "A couch", 105.00, 8),
@@ -85,8 +115,15 @@ class Item{
     };
 }
 class AddRemoveItem{
-    int ItemID;
 
+    void AddItem(Item[] items, int ID, String name, String desc,double price, int Qty){
+        Item newItem = new Item(ID, name, desc, price, Qty);
+
+        items = Arrays.copyOf(items, items.length + 1);
+        items[items.length - 1] = newItem;
+        StaffBackend backend = new StaffBackend();
+        backend.Backend(Item.items);
+    }
     void RemoveItem(Item[] items, int ID){
         for (int i = 0; i < items.length; i++) {
             if (items[i].itemID == ID) {
@@ -99,17 +136,116 @@ class AddRemoveItem{
                 break; // Exit the loop once the item is removed
             }
         }
+        StaffBackend backend = new StaffBackend();
+        backend.Backend(Item.items);
     }
 }
 class EditItem{
-    int itemID;
-    String itemName, description;
-    double itemPrice;
+    static void editItemInfo(Item[] items, int itemId, String newName, String newDesc, double newPrice) {
+        for (Item item : items) {
+            if (item.itemID == itemId) {
+                item.itemName = newName;
+                item.description = newDesc;
+                item.price = newPrice;
+                System.out.println("Item information updated successfully.");
+                return; // Once the item is found and updated, exit the loop
+            }
+        }
+        System.out.println("Item with ID " + itemId + " not found.");
+    }
+}
+class RefillStock{
+    static void refillItemQuantity(Item[] items, int itemId, int additionalQty) {
+        for (Item item : items) {
+            if (item.itemID == itemId) {
+                item.itemQty += additionalQty;
+                System.out.println("Item quantity refilled successfully.");
+                return; // Once the item is found and quantity refilled, exit the loop
+            }
+        }
+        System.out.println("Item with ID " + itemId + " not found.");
+    }
+}
+class ItemInteraction{
+    void InteractionRequest(Item[] items, int ID, int request){
+        switch (request) {
+            case 1:
+                // Implement buy item logic (you can add a method for this)
+                if(CheckStock.checkStock(ID)){
+                    Purchase transaction = new Purchase();
+                    transaction.PurchaseItem(Item.items, ID);
+                }
+                else{
+                    System.out.println("Item not in stock. ");
+                }
+                break;
+            case 2:
+                // Implement add to wishlist logic (you can add a method for this)
+                Item.addToWishlist(ID);
+                break;
+            case 3:
+                // Implement like item logic (you can add a method for this)
+                System.out.println("Item liked!");
+                break;
+            case 4:
+                // User wants to return to the item list
+                CustomerFrontend frontend = new CustomerFrontend();
+                frontend.Frontend(items);
+                break;
+            default:
+                System.out.println("Invalid choice");
+        }
+    }
+}
+class Purchase{
+    void PurchaseItem(Item[] items, int ID) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Select payment method:");
+        System.out.println("1. Credit card");
+        System.out.println("2. Debit card");
+
+        int paymentMethod = scanner.nextInt();
+
+        if (paymentMethod == 1 || paymentMethod == 2) {
+            System.out.println("Enter card number:");
+            long cardNumber = scanner.nextLong();
+
+            // Pretend to verify the card number (you may implement a real verification logic here)
+            boolean isCardVerified = verifyCardNumber(cardNumber);
+
+            if (isCardVerified) {
+                System.out.println("Purchase successful!");
+
+                // Decrease the item quantity by 1
+            } else {
+                System.out.println("Card verification failed. Purchase unsuccessful.");
+            }
+        } else {
+            System.out.println("Invalid payment method selected.");
+        }
+    }
+
+    private boolean verifyCardNumber(long cardNumber) {
+        // Pretend verification logic
+        // You may implement a real verification logic based on your requirements
+        return cardNumber > 0;  // Just a simple example
+    }
+}
+class CheckStock{
+    public static boolean checkStock(int itemID) {
+        for (Item item : Item.items) {
+            if (item.itemID == itemID) {
+                return item.itemQty > 0;
+            }
+        }
+        // Item not found, you might want to handle this case based on your requirements
+        System.out.println("Item with ID " + itemID + " not found in the items list.");
+        return false;
+    }
 }
 
-class RefillStock{
-    int itemID, itemQty;
-}
+
 
 //Collection of Display classes
 class CustomerFrontend {
@@ -124,23 +260,64 @@ class CustomerFrontend {
         }
 
             System.out.println("For more information or to purchase or wishlist an item, please enter the items ID number ");
-            System.out.println("To contact a staff member, press 0 \n");
+            System.out.println("For more options, press 0 \n");
             int switchSelect = takeIn.nextInt();
 
             if(switchSelect != 0) {
                 ProductView(Item.items, switchSelect);
             }
             else {
-                Chatbox message = new Chatbox();
-                message.messageBody();
+                System.out.println("To chat with support staff, press 1");
+                System.out.println("To view your wishlist, press 2");
+                int nextSelect = takeIn.nextInt();
+
+                switch (nextSelect){
+                    case 1:
+                        Chatbox message = new Chatbox();
+                        message.messageBody();
+                        break;
+                    case 2:
+                        for (Item item : Item.wishlist) {
+                            System.out.println("Item ID: " + item.itemID);
+                            System.out.println("Item Name: " + item.itemName);
+                            System.out.println();
+                        }
+                        break;
+                    default:
+                        System.out.println("Not a valid input");
+                        break;
+                }
             }
 
     }
 
     void ProductView(Item[] items, int ID){
-        System.out.println("Name: " + items[ID].itemName);
-        System.out.println("Description: " + items[ID].description);
-        System.out.println("Name: " + items[ID].price);
+        Optional<Item> selectedItem = Arrays.stream(items)
+                .filter(item -> item.itemID == ID)
+                .findFirst();
+
+        if (selectedItem.isPresent()) {
+            // Item found, display details
+            System.out.println("Name: " + selectedItem.get().itemName);
+            System.out.println("Description: " + selectedItem.get().description);
+            System.out.println("Price: " + selectedItem.get().price);
+
+            System.out.println("\nOptions:");
+            System.out.println("1. Buy the item");
+            System.out.println("2. Add to wishlist");
+            System.out.println("3. Like the item");
+            System.out.println("4. Return to item list");
+
+            Scanner scanner = new Scanner(System.in);
+            int userChoice = scanner.nextInt();
+            ItemInteraction request = new ItemInteraction();
+            request.InteractionRequest(Item.items, ID, userChoice);
+
+
+        } else {
+            // Item not found
+            System.out.println("Item with ID " + ID + " not found.");
+        }
 
     }
 }
@@ -158,18 +335,79 @@ class StaffBackend{
         switch (staffSelect){
             case 1:
             case 2:
-                Add_Remove(Item.items, staffSelect);
+                Add_Remove(staffSelect);
+                break;
+            case 3:
+                Edit_Item_Info();
+                break;
+            case 4:
+                Refill_Stock();
+                break;
+            default:
+                System.out.println("Not a valid input");
+                break;
         };
     }
-    void Add_Remove(Item[] items, int select){
+    void Add_Remove(int select){
+        Scanner takeIn = new Scanner(System.in);
+
         if(select == 1){
-            System.out.println("Enter info for new Item: ");
+            System.out.println("--Enter info for new Item-- ");
+            System.out.println("Enter Item ID: ");
+            int ID = takeIn.nextInt();
+            takeIn.nextLine();
+            System.out.println("Enter Item Name: ");
+            String name = takeIn.nextLine();
+            System.out.println("Enter Item Description: ");
+            String desc = takeIn.nextLine();
+            System.out.println("Enter Item Price: ");
+            double price = takeIn.nextDouble();
+            System.out.println("Enter Item Quantity: ");
+            int Qty = takeIn.nextInt();
+
+            AddRemoveItem add = new AddRemoveItem();
+            add.AddItem(Item.items, ID, name, desc, price, Qty);
         }
         else if(select == 2){
-            System.out.println("Which item to remove?");
+            System.out.println("Enter the ID of the item to be removed: ");
+            int ID = takeIn.nextInt();
+            AddRemoveItem remove = new AddRemoveItem();
+            remove.RemoveItem(Item.items, ID);
+            System.out.println("Item removed succesfully");
         }
     }
+    void Edit_Item_Info(){
+        Scanner takeIn = new Scanner(System.in);
+
+        System.out.println("Enter the ID of the item to be edited: ");
+        int ID = takeIn.nextInt();
+        takeIn.nextLine(); // Consume the newline character
+        System.out.println("Enter new Item Name: ");
+        String newName = takeIn.nextLine();
+        System.out.println("Enter new Item Description: ");
+        String newDesc = takeIn.nextLine();
+        System.out.println("Enter new Item Price: ");
+        double newPrice = takeIn.nextDouble();
+
+        EditItem.editItemInfo(Item.items, ID, newName, newDesc, newPrice);
+        StaffBackend backend = new StaffBackend();
+        backend.Backend(Item.items);
+    }
+    void Refill_Stock(){
+        Scanner takeIn = new Scanner(System.in);
+
+        System.out.println("Enter the ID of the item to refill: ");
+        int ID = takeIn.nextInt();
+        System.out.println("Enter the quantity to add: ");
+        int additionalQty = takeIn.nextInt();
+
+        RefillStock.refillItemQuantity(Item.items, ID, additionalQty);
+        StaffBackend backend = new StaffBackend();
+        backend.Backend(Item.items);
+    }
 }
+
+
 
 //Collection of classes for sending messages
 class Chatbox{
@@ -183,7 +421,6 @@ class Chatbox{
         MessageCoordinator send = new MessageCoordinator(message);
     }
 }
-
 class MessageCoordinator{
     String message;
 
@@ -196,6 +433,9 @@ class MessageCoordinator{
         System.out.println("Staff: Let me look into that for you, I can assist you shortly.");
     }
 }
+
+
+
 //Main Class
 public class Main {
     public static void main(String[] args) {
@@ -211,6 +451,9 @@ public class Main {
         else if(userStatus.equals("Staff")){
             StaffBackend backend = new StaffBackend();
             backend.Backend(Item.items);
+        }
+        else if(userStatus.equals("CEO")){
+            Report report = new Report();
         }
 
     }
